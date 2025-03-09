8b2f9a1546d8740b0aa9ae225e2875d35e0ff260
arseniizyk <arseniizyk@gmail.com>
2025-03-09 18:10:22 +0300

    Function GetItemPairs

--- internal/app/app.go
+++ internal/app/app.go
@@ -34,12 +34,23 @@ func New() (*App, error) {
 
 func (a *App) Run() error {
 	// minimum 100 players and maximum 25k
-	meta, err := a.svc.GetItemsMeta("2025-02", 100)
+	// meta, err := a.svc.GetItemsMeta("2025-02", 100)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// file, _ := os.Create("meta/meta.json")
+	// defer file.Close()
+	// formatted, _ := json.MarshalIndent(meta, "", "  ")
+	// file.Write(formatted)
+	// return nil
+
+	meta, err := a.svc.GetItemPairsMeta("2025-02", 10000)
 	if err != nil {
 		return err
 	}
 
-	file, _ := os.Create("meta/meta.json")
+	file, _ := os.Create("metapairs/2025-02/meta10k.json")
 	defer file.Close()
 	formatted, _ := json.MarshalIndent(meta, "", "  ")
 	file.Write(formatted)
---
+++ internal/pkg/coc/meta.go
@@ -0,0 +1,3 @@
+// here should be GetItemsMeta and GetItemPairsMeta functions
+
+package coc
