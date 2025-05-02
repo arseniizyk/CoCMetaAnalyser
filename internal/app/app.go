e729747e766e17bace8123daadb995864f4b0410
arseniizyk <arseniizyk@gmail.com>
2025-05-02 18:08:14 +0300

    April Updates

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -33,25 +34,32 @@ func New() (*App, error) {
 }
 
 func (a *App) Run() error {
+	const Date = "2025-04"
 	// minimum 100 players and maximum 25k
-	// meta, err := a.svc.GetMetaItems(10000, "2025-03")
-	// if err != nil {
-	// 	return err
-	// }
 
-	// file, _ := os.Create("meta/2025-03/meta10k.json")
-	// defer file.Close()
-	// formatted, _ := json.MarshalIndent(meta, "", "  ")
-	// file.Write(formatted)
+	var (
+		metaPath      = fmt.Sprintf("meta/%s/meta10k.json", Date)
+		metaPairsPath = fmt.Sprintf("metapairs/%s/meta10k.json", Date)
+	)
 
-	// meta, err = a.svc.GetMetaItemPairs(10000, "2025-03")
-	// if err != nil {
-	// 	return err
-	// }
+	meta, err := a.svc.GetMetaItems(10000, Date)
+	if err != nil {
+		return err
+	}
 
-	// file, _ = os.Create("metapairs/2025-03/meta10k.json")
-	// defer file.Close()
-	// formatted, _ = json.MarshalIndent(meta, "", "  ")
-	// file.Write(formatted)
-	// return nil
+	file, _ := os.Create(metaPath)
+	defer file.Close()
+	formatted, _ := json.MarshalIndent(meta, "", "  ")
+	file.Write(formatted)
+
+	meta, err = a.svc.GetMetaItemPairs(100, Date)
+	if err != nil {
+		return err
+	}
+
+	file, _ = os.Create(metaPairsPath)
+	defer file.Close()
+	formatted, _ = json.MarshalIndent(meta, "", "  ")
+	file.Write(formatted)
+	return nil
 }
