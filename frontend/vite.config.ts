0c9b356696c1028b5838ced51cf35601b4ae6cc9
arseniizyk <arseniizyk@gmail.com>
2025-08-27 19:34:03 +0300

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/cocmetaanalyser/', // <-- добавь это
+    base: '/cocmetaanalyser/',
     plugins: [react()],
 })
